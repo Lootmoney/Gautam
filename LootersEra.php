dfc72f76aefcf77e67c599ba97b014d46ca7fa58
Lootmoney <90970607+Lootmoney@users.noreply.github.com>
2021-09-18 18:52:25 +0530

    Add files via upload

---
+++ LootersEra.php
@@ -0,0 +1,112 @@
+<html>
+    <head>
+        <meta name="robots" content="index">
+        <meta name="viewport" content="width=device-width">
+        <title>!! Crypto-X-Coder !!</title>
+        <link href='https://fonts.googleapis.com/css?family=Black Ops One' rel='stylesheet'>       
+        <link href='https://fonts.googleapis.com/css?family=Secular One' rel='stylesheet'>        
+        <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'>        
+        <link href='https://fonts.googleapis.com/css?family=Mouse Memoirs' rel='stylesheet'>
+        
+    <style>
+
+body {
+    font-family:'Acme';
+    text-align:center;
+    padding:7px 0px;
+    background:#ffff00;
+    letter-spacing:2px;}
+
+hr { 
+    border:0;
+    height:1px;
+    background-image:linear-gradient(
+    to right,#33ffff,blue,#33ffff);}
+
+.text {
+    font-size:16px;
+    color:#8e2de2;
+    outline:0;
+    border-radius:5px;
+    font-family:'Merriweather';
+    text-align:center;
+    padding:8px 15px;
+    height:45px;
+    border:solid 1px #ffffff;
+    margin-bottom:20px;
+    box-shadow:10px 10px 20px 
+    rgba(0,0,0,0.1);width:85%}
+
+.head {
+    color: #ff0033;
+    font-family:'Black Ops One';
+    width: 360px;
+    text-align:left;
+    padding: 3px 3px;
+    background: #00ffff;
+    border-radius: 2px;
+    border: solid #6633ff 2px ;
+    overflow: hidden;}
+
+.text4 {
+    font-size:21px;
+    color:white;
+    background:#0000ff;
+    outline:0;
+    border-radius:5px ;
+    font-family:'Secular One';
+    text-align:center;
+    padding:2px 3px;
+    height:20px;
+    border:solid 2px red;
+    margin-bottom:2px;
+    box-shadow:10px 10px 20px 
+    rgba(0,0,0,0.1);width:75% ;}
+
+.submit {
+    font-size:21px;
+    color:#ffffff;
+    background:linear-gradient(to 
+    top right,#ff00cc, #ff0000);
+    outline:0;
+    border-radius: 200px;
+    font-family:'Mouse Memoirs';
+    text-align:center;
+    padding:8px 5px;
+    height:45px;
+    border:solid 2px #00ffff;
+    margin-bottom:20px;
+    box-shadow:10px 10px 20px 
+    rgba(0,0,0,0.1);width:40%}
+
+.suraj {
+    width: 350px;
+    background: #0000ff;
+    border-radius: 20px;
+    border: solid red 2px ;
+    overflow: hidden;}
+  
+  </style></head>
+
+<hr><center>
+<font class="head" size="6">Swiggy Game Bypass</font>
+</center><hr>
+
+<br>
+<br>
+<br>
+<center>
+<form method='get' action='https://techssing.com/sw/'>
+
+<br><Div>
+<input type='number' class='text'name='num' placeholder='Enter Registered Number' required>
+<input type='submit' class='submit' name='submit' value='Submit'>
+<br></form>
+</center>
+
+<hr>
+<br>
+<br>
+<center><script type='text/javascript'>(function() {var script=document.createElement('script');script.type='text/javascript';script.async =true;script.src='//telegram.im/widget-button/index.php?id=@Oet_Official';document.getElementsByTagName('head')[0].appendChild(script);})();</script><a href='tg://resolve?domain=Oet_Official' target='_blank' class='telegramim_button telegramim_shadow telegramim_pulse' style='font-size:18px;font-family:Merriweather; max-width:400px;background:#0033FF;box-shadow:1px 1px 5px #00ff00;color:#ffffff;border-radius:20px;' title='Contact Me><i></i>Join Our Telegram Channel<small><span class='telegramim_count' data-for='@Oet_Official'>...</span></small></a></center>
+<br>
+</body></html>
